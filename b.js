6b7b2591b0d02a1dbce07a4ccd181f03cdd29854
Divyansh Bansal <54841396+Divyansh4721@users.noreply.github.com>
2023-10-03 13:42:32 +0530

    Add files via upload

--- b.js
+++ b.js
@@ -1,9 +1,13 @@
 function createVCard() {
+    let starttext = document.getElementsByTagName("input")[0].value;
     let inputText = document.getElementsByTagName("textarea")[0].value;
     let lines = inputText.trim().split('\n');
     let vCardStrings = [];
     for (let i = 0; i < lines.length; i += 2) {
         let name = lines[i].trim();
+        if (starttext) {
+            name = starttext + "-" + name;
+        }
         let phone = lines[i + 1].trim();
         if (name && phone) {
             let vCard = [
