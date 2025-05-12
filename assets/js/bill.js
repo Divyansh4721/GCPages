58391c0f3f4c8c9d27eae45b5a4bd63dd5af9c0b
Divyansh4721 <divyanshbansal41@gmail.com>
2025-05-12 13:59:33 +0530

    major update design

--- assets/js/bill.js
+++ assets/js/bill.js
@@ -29,7 +29,7 @@ function printTable() {
 
             <head>
                 <title>Print Table</title>
-                <link rel="stylesheet" href="billprint.css">
+                <link rel="stylesheet" href="assets/css/billprint.css">
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"
                     integrity="sha512-BNaRQnYJYiPSqHHDb58B0yaPfCu+Wgds8Gp/gU33kqBtgNS4tSPHuGibyoeqMV/TJlSKda6FXzoEyYGjTe+vXA=="
                     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
--- assets/js/dimossbill.js
+++ assets/js/dimossbill.js
@@ -14,7 +14,7 @@ function printTable() {
 
             <head>
                 <title>Print Table</title>
-                <link rel="stylesheet" href="dimossbillprint.css">
+                <link rel="stylesheet" href="assets/css/dimossbillprint.css">
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"
                     integrity="sha512-BNaRQnYJYiPSqHHDb58B0yaPfCu+Wgds8Gp/gU33kqBtgNS4tSPHuGibyoeqMV/TJlSKda6FXzoEyYGjTe+vXA=="
                     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
